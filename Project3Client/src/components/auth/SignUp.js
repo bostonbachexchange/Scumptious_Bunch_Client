292c5c5573338d693d68ace907870e77d6ad8d02
Alys <alice.g.cooper@gmail.com>
2022-08-03 11:54:03 -0400

    check packagelock

--- Project3Client/src/components/auth/SignUp.js
+++ Project3Client/src/components/auth/SignUp.js
@@ -9,15 +9,6 @@ import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 
 const SignUp = (props) => {
-	// constructor(props) {
-	// 	super(props)
-
-	// 	this.state = {
-	// 		email: '',
-	// 		password: '',
-	// 		passwordConfirmation: '',
-	// 	}
-	// }    
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [passwordConfirmation, setPasswordConfirmation] = useState('')
@@ -29,7 +20,7 @@ const SignUp = (props) => {
 
 		const { msgAlert, setUser } = props
 
-        const credentials = {email, password, passwordConfirmation}
+        const credentials = {name, email, password, passwordConfirmation}
 
 		signUp(credentials)
 			.then(() => signIn(credentials))
