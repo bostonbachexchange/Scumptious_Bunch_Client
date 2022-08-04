8ee6e5adc71c27c18570b9d17218df88f1f3574d
Jacob Clapper <105309528+bostonbachexchange@users.noreply.github.com>
2022-08-04 14:51:38 -0400

    Added axios routes for  Api/profiles.js

---
+++ Project3Client/src/api/profiles.js
@@ -0,0 +1,56 @@
+import apiUrl from "../apiConfig";
+import axios from "axios";
+
+// READ => SHOW
+export const getOneProfile = (id) => {
+    return axios(`${apiUrl}/profile/${id}`)
+}
+
+// CREATE
+export const createProfile = (user, newProfile) => {
+    console.log('createProfile in API was hit')
+    // in our createProfile form, we're building an object
+    // when we pass that object into the api createProfile function, it's going to look like the profile in our database
+    // we're going to refer to this as newProfile
+    console.log('this is user:', user)
+    console.log('this is newProfile', newProfile)
+	return axios({
+		// POST /profile/<user_id>
+		url: apiUrl + `/profile/${id}`,
+		method: 'POST',
+		headers: {
+			Authorization: `Token token=${user.token}`,
+		},
+        // since newProfile and profile have the same fields, we just have to do this!
+		data: { profile: newProfile } ,
+	})
+}
+
+
+// UPDATE
+export const updateProfile = (user, updatedProfile) => {
+    // console.log('updateProfile in API was hit')
+    console.log('this is updated profile', updatedProfile)
+	return axios({
+		url: `${apiUrl}/updateProfile/`,
+		method: 'PATCH',
+		headers: {
+			Authorization: `Token token=${user.token}`,
+		},
+        // since newPet and pet have the same fields, we just have to do this!
+		data: { profile: updatedProfile } ,
+	})
+}
+
+// DELETE
+export const deleteProfile = (user) => {
+    // 
+    console.log('this is deleted service id')
+	return axios({
+		url: `${apiUrl}/profile}`,
+		method: 'DELETE',
+		headers: {
+			Authorization: `Token token=${user.token}`,
+		}
+	})
+}
