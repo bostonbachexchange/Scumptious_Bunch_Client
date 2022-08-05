b8b22a633903683b5e6c4433f3968a6c18276ab4
Alys <alice.g.cooper@gmail.com>
2022-08-05 14:31:08 -0400

    Fix ShowFreelancer view

--- Project3Client/src/components/freelancers/ShowFreelancer.js
+++ Project3Client/src/components/freelancers/ShowFreelancer.js
@@ -7,6 +7,7 @@ import { useParams, useNavigate, Link } from 'react-router-dom'
 import { Container, Card, Button } from 'react-bootstrap'
 
 import LoadingScreen from '../shared/LoadingScreen'
+// this api call gets us an array with [0] as user and [1] as services
 import { getServicesByUser } from '../../api/freelancers'
 import messages from '../shared/AutoDismissAlert/messages'
 
@@ -30,8 +31,8 @@ const ShowFreelancer = (props) => {
     // this returns the services that we want to show (from the desired user/freelancer)
     useEffect(() => {
         getServicesByUser(id)
-            // .then(res => console.log('here is res.data.services[0]', res.data.services[0]))
-            .then(res => { setService(res.data.services[0])})
+            //.then(res => console.log('here is res.data', res.data))
+            .then(res => { setService(res.data.services[1])})
             .catch(err => {
                 msgAlert({
                     heading: 'Error getting service',
@@ -43,22 +44,24 @@ const ShowFreelancer = (props) => {
     }, [])
     console.log('here is service', service)
     // IDEALLY, this returns the desired user information that we want to show as well as the services
-    // useEffect(() => {
-    //     getServicesByUser(id)
-    //         .then(res => {
-    //             console.log('here is res.data.services[0]', res.data.services[0])
-    //             // setFreelancer(res.data.service[0])
-    //         })
-    //         .catch(err => {                   
-    //             msgAlert({
-    //                 heading: 'Error getting freelancer',
-    //                 message: messages.getFreelancersFailure,
-    //                 variant: 'danger'
-    //             })
-    //             navigate('/')
-    //         })
-    // }, [])
-    // console.log('here is the set freelancer', freelancer)
+    useEffect(() => {
+        getServicesByUser(id)
+            .then(res => {
+                console.log('here is res.data.services[0]', res.data.services[0])
+                let serviceFreelancer = res.data.services[0]
+                console.log('here is serviceFreelancer', serviceFreelancer)
+                setFreelancer(serviceFreelancer)
+            })
+            .catch(err => {                   
+                msgAlert({
+                    heading: 'Error getting freelancer',
+                    message: messages.getFreelancersFailure,
+                    variant: 'danger'
+                })
+                navigate('/')
+            })
+    }, [])
+    console.log('here is the set freelancer', freelancer)
     if (!service) {
         return <LoadingScreen />
     }
@@ -78,24 +81,26 @@ const ShowFreelancer = (props) => {
             </Card>
         </>
     ))
-    // console.log('here is the freelancer variable', freelancer)
+    console.log('here is the freelancer variable', freelancer)
     return (
         <>
+        {freelancer && 
             <div>
-                {/* <h2>Services By {freelancer.name}</h2>
-                <h5>Email: {freelancer.email}</h5>  */}
-                {/* { (freelancer.profile[0]) ? (
+                <h2>Services By {freelancer.name}</h2>
+                <h5>Email: {freelancer.email}</h5> 
+                { (freelancer.profile) ? (
                                 <div><small>
-                                Bio: { freelancer.profile[0].aboutMe }
+                                Bio: { freelancer.profile.aboutMe }
                                 </small></div>
                                 ) : (
                                     <p>user does not have a profile yet.</p>
-                            )} */}
-            </div>
+                            )}
+            </div> }
             <Container className='fluid-services'>
                 {freelancerServices}
-            </Container>
+            </Container> 
         </>
+        
     )
 }
 
