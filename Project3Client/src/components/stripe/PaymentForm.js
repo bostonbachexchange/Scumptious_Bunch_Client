4377c599ce9a0faf578487dbc7579fe8482ae78c
hrmoyes <hrmoyes@gmail.com>
2022-08-08 09:16:34 -0500

    React frontend done

--- Project3Client/src/components/stripe/PaymentForm.js
+++ Project3Client/src/components/stripe/PaymentForm.js
@@ -42,7 +42,7 @@ export default function PaymentForm() {
     if(!error) {
         try {
             const { id } = paymentMethod
-            const response = await axios.post('http://localhost:3000/payment', {
+            const response = await axios.post('http://localhost:8000/payment', {
                 amount: 1000,
                 id
             })
